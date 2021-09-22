8929eecb09435b28c26db85a19b7829202761185
yan <gavriliuk.stas@gmail.com>
2021-09-22 15:19:10 +0300

    change first commit field to test value

--- MainTest/Schemas/PC1Page/PC1Page.js
+++ MainTest/Schemas/PC1Page/PC1Page.js
@@ -6,7 +6,7 @@ define("PC1Page", [], function() {
 				dataValueType: this.Terrasoft.DataValueType.TEXT,
 				type: this.Terrasoft.ViewModelColumnType.VIRTUAL_COLUMN,
 				value: "test-field-value",
-				caption: "tesdt-filed-stas"
+				caption: "tesdt-filed-stas-change"
 			}
 		},
 		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
